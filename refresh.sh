622c50c6908335f88d827a81ec313f54f9419c36
Derrik Diener <142091676+soltros@users.noreply.github.com>
2026-01-13 05:00:32 -0500

    Clarify script usage with an explanatory comment
    Add comment to clarify script usage after docker compose down

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,5 @@
+### Run this after docker compose down to re-build the container and redeploy it easily.
+
 sudo docker rmi whiteboard-whiteboard:latest --force
 sudo docker build .
 sudo docker compose up -d
